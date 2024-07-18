946a7515e15da32043a7214439b7c5c104e8f9f1
NoorShaik683 <106299708+NoorShaik683@users.noreply.github.com>
2024-07-18 18:27:35 +0530

    Create subarray-sum-equals-k.py

---
+++ Array/subarray-sum-equals-k.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        #bruteforce
+        # c=0
+        # for i in range(len(nums)):
+        #     curr_sum=0
+        #     for j in range(i, len(nums)):
+        #         curr_sum+=nums[j]
+        #         if curr_sum==k:
+        #             c+=1
+        #         elif curr_sum>k:
+        #             break
+        # return c
+
+        d={0:1}
+        c=0
+        curr_sum = 0
+        for i in nums:
+            curr_sum+=i
+            temp = curr_sum-k
+            if d.get(temp):
+                c+=d[temp]
+            d[curr_sum]=d.get(curr_sum,0)+1
+        return c
